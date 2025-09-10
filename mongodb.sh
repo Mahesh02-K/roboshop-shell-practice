3d5a59585b66913b6ab411f9e52be9034063b5ed
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-10 19:13:06 +0530

    shell scripting

--- mongodb.sh
+++ mongodb.sh
@@ -42,7 +42,7 @@ systemctl enable mongod &>>$LOG_FILE
 systemctl start mongod &>>$LOG_FILE
 VERIFY $? "Starting Mongodb"
 
-sed -i "s/127.0.0.0/0.0.0.0/g" /etc/mongod.conf
+sed -i "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf
 VERIFY $? "Editing Mongod config file to enable remote connections"
 
 systemctl restart mongod &>>$LOG_FILE
