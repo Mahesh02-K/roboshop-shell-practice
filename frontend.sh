f7d6f21e85131f6c5ee8384c8809c57ac4565546
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-10 18:42:16 +0530

    shell scripting

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$START_TIME=$(date +%s)
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -13,46 +13,46 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
-echo "Script started executing at : $(date)"
+echo "Script started executing at : $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ] #check root privileges
 then
-    echo -e "$R ERROR :::$N PLEASE RUN WITH ROOT ACCESS"
+    echo -e "$R ERROR :::$N PLEASE RUN WITH ROOT ACCESS" | tee -a $LOG_FILE
     exit 1 
 else
-    echo -e "$G You are running with root access $N"
+    echo -e "$G You are running with root access $N" | tee -a $LOG_FILE
 fi
 
-VERIFY(){ #verify function 
+VERIFY(){ 
     if [ $1 -eq 0 ]
     then
-        echo -e "$2 is ... $G SUCCESS $N"
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo -e "$2 is ... $R FAILURE $N"
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
 }
 
-dnf module disable nginx -y
+dnf module disable nginx -y &>>$LOG_FILE
 VERIFY $? "Disabling default nginx"
 
-dnf module enable nginx:1.24 -y
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
 VERIFY $? "Enabling nginx:1.24"
 
-dnf install nginx -y
+dnf install nginx -y &>>$LOG_FILE
 VERIFY $? "Installing nginx"
 
-systemctl enable nginx 
+systemctl enable nginx &>>$LOG_FILE
 systemctl start nginx 
 VERIFY $? "Starting nginx"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VERIFY $? "Removing Default Content"
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 VERIFY $? "Downloading Frontend Content"
 
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VERIFY $? "Unzipping frontend Content"
 
 rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
@@ -61,12 +61,12 @@ VALIDATE $? "Remove default nginx conf"
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
 VERIFY $? "Copying Nginx conf file"
 
-systemctl restart nginx 
+systemctl restart nginx &>>$LOG_FILE
 VERIFY $? "Restarting nginx"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-echo -e "Script execution completed successfully, time taken is $Y $TOTAL_TIME secs $N"
+echo -e "Script execution completed successfully, time taken is $Y $TOTAL_TIME secs $N" | tee -a $LOG_FILE
 
 
 
