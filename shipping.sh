1c00ad8d7bc2dfeccbd54622da173a3fd57a0d94
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-12 14:04:09 +0530

    Shell scripting

--- shipping.sh
+++ shipping.sh
@@ -80,9 +80,9 @@ VERIFY $? "Installing Mysql"
 mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD cities  < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD cities  < /app/db/app-user.sql &>>$LOG_FILE
-    mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD cities < /app/db/master-data.sql &>>$LOG_FILE
+    mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
+    mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
     VERIFY $? "Loading data"
 else
     echo -e "$Y Data is already Loaded $N" | tee -a $LOGS_FILE
