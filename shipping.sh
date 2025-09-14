6dc2463ba56e28310a88013a05cd83bf5e527066
Mahesh02-K <kakuturumahesh@gmail.com>
2025-09-14 17:32:38 +0530

    Shell Scripting

--- shipping.sh
+++ shipping.sh
@@ -79,14 +79,16 @@ VERIFY $? "Installing Mysql"
 mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD -e 'use cities' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
-    VERIFY $? "Loading schema"
+    # mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    # VERIFY $? "Loading schema"
 
-    mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
-    VERIFY $? "Creating app user"
+    # mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql &>>$LOG_FILE
+    # VERIFY $? "Creating app user"
 
-    mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
-    VERIFY $? "Loading data"
+    # mysql -h mysql.kakuturu.store -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    # VERIFY $? "Loading data"
+        mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+        VERIFY $? "Loading data"
 else
     echo -e "$Y Data is already Loaded $N" | tee -a $LOGS_FILE
 fi
